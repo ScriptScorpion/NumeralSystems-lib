5b4fe6d2977441b64117cc545c564e1ba15657fc
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-09-02 19:25:28 +0300

    negative number handling

--- hex.h
+++ hex.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <stdlib.h>
 // basicly algorithm doing / 16 and reminder is storing in array and then prints all backwards
-static inline char *ToHex(unsigned int num) {
+static inline char *ToHex(int num) {
+     if (num < 0) {
+          return 0;
+     }
      unsigned char *hex_num = (unsigned char*)calloc(sizeof(unsigned char) * 32, sizeof(unsigned char)); // we creating with unsigned char to insure no strange characters are going to output
      char *res = (char*)calloc(sizeof(char) * 32, sizeof(char)); 
      int i = 0; // iteration we are currently elapsed
