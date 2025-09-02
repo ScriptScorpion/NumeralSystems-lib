df1b01f68884c51ea76a4136880aa001df813fc1
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2025-09-02 16:56:47 +0300

    refactoring

--- binary.h
+++ binary.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <stdlib.h>
 // basicly algorithm doing / 2 and reminder is storing in array and then prints all backwards
-static inline unsigned int ToBinary(unsigned int num) {
+static inline int ToBinary(unsigned int num) {
     unsigned int *binary_num = (unsigned int*)malloc(sizeof(unsigned int) * 32);
-    unsigned int res = 0;
+    int res = 0;
     int i = 0; // iteration we are currently elapsed
     while (num > 0) {
         binary_num[i] = num % 2;
